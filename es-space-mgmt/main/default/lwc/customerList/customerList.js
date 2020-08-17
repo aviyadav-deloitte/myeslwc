b61f0c00e4b4a0295b72e5dd9fdc198830706a2f
Avinash Yadav <aviyadav@deloitte.com>
2020-08-17 14:48:28 -0400

    Added apex class controller

--- es-space-mgmt/main/default/lwc/customerList/customerList.js
+++ es-space-mgmt/main/default/lwc/customerList/customerList.js
@@ -1,7 +1,30 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import getCustomerList from '@salesforce/apex/reservationManagerController.getCustomerList';
 
 export default class CustomerList extends LightningElement {
 
-    customers = [ { Id : 1}, { Id : 2}, { Id : 3}];
+    customers = [];
+
+    @api sobject = 'Lead';
+    errorMsg;
+    msgForUser;
+    wiredRecords;
+
+
+    @wire(getCustomerList, { sObjectType: '$sobject' })
+    wiredCustomerData(value) {
+        console.log(JSON.stringify(value));
+        this.wiredRecords = value;
+        if (value.error) {
+            this.errorMsg = value.error;
+            this.msgForUser = 'There was an issue loading customers.';
+        } else if (value.data) {
+            this.customers = value.data;
+        }
+    }
+
+    handleSelect(event) {
+        console.log(event.detail);
+    }
     
 }
